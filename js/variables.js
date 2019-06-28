ce483848f2fbec9ed429d5dc4703601a9eba3776
Tim Thomas <tim@example.com>
2019-06-28 11:37:20 -0400

    end of class

---
+++ js/variables.js
@@ -0,0 +1,10 @@
+let x,y,z;
+x=10;
+y = '10';
+z=30;
+console.log(`x is ${typeof x}`);
+console.log(`y is ${typeof x}`);
+console.log(`z is ${typeof x}`);
+var newX = x++;
+console.log('The comparison of x==y is:', (x==y));
+let timeInMs = Date.now();
